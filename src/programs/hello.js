5c66d36de2f41e0f2aabbf97d0c624e3aa5e5626
Justin Liang <justinliang1020@gmail.com>
2025-08-17 00:20:48 -0400

    add hello program

---
+++ src/programs/hello.js
@@ -0,0 +1,29 @@
+import { ProgramBase } from "../programBase.js";
+import { h, text } from "../packages/hyperapp/index.js";
+
+/**
+ * @typedef ProgramState
+ * @property {String} hello
+ */
+
+export class Program extends ProgramBase {
+  constructor() {
+    super();
+    /** @type {ProgramState} */
+    this.defaultState = {
+      hello: "hello",
+    };
+    /** @type {AllowedConnection[]} */
+    this.allowedConnections = [];
+    this.view = this.#main;
+    this.subscriptions = () => {
+      return [];
+    };
+  }
+
+  /**
+   * @param {ProgramState} state
+   * @returns {import("hyperapp").ElementVNode<ProgramState>}
+   */
+  #main = (state) => h("div", {}, [h("h3", {}, text(state.hello))]);
+}
