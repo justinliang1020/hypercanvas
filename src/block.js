c27d499af7944969da289f1a261c97149b6ffb32
Justin Liang <justinliang1020@gmail.com>
2025-09-29 23:45:30 -0400

    enable pointer events on blocks at all times

--- src/block.js
+++ src/block.js
@@ -195,8 +195,6 @@ export function block(state) {
           "div",
           {
             style: {
-              pointerEvents:
-                isEditing || currentPage.isInteractMode ? null : "none",
               width: "100%",
               height: "100%",
               overflow: "hidden",
