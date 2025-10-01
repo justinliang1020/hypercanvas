e47869581522778b52e894173801dd01abc1e504
Justin Liang <justinliang1020@gmail.com>
2025-09-30 23:59:38 -0400

    Revert "enable pointer events on blocks at all times"
    This reverts commit c27d499af7944969da289f1a261c97149b6ffb32.

--- src/block.js
+++ src/block.js
@@ -195,6 +195,8 @@ export function block(state) {
           "div",
           {
             style: {
+              pointerEvents:
+                isEditing || currentPage.isInteractMode ? null : "none",
               width: "100%",
               height: "100%",
               overflow: "hidden",
