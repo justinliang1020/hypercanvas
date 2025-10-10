1ce4d3dbab002dd65525783a24be7c27f874718a
Justin Liang <justinliang1020@gmail.com>
2025-10-10 18:54:50 -0400

    convert types.js -> index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,104 @@
+interface Block {
+  id: number;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  zIndex: number;
+  program: string;
+}
+
+interface Page {
+  id: string;
+  name: string;
+  blocks: Block[];
+  offsetX: number;
+  offsetY: number;
+  zoom: number;
+  mouseX: number;
+  mouseY: number;
+  cursorStyle: string;
+  isViewportDragging: boolean;
+  isTextEditorFocused: boolean;
+  isInteractMode: boolean;
+  selectedIds: number[];
+  previewSelectedIds: number[];
+  editingId: number | null;
+  hoveringId: number | null;
+  resizing: ResizeState | null;
+  dragStart: DragState | null;
+  selectionBox: SelectionBoxState | null;
+  state: any;
+  css: String;
+}
+
+interface Memento {
+  pages: Page[];
+  currentPageId: string;
+}
+
+interface MementoManager {
+  undoStack: Memento[];
+  redoStack: Memento[];
+  maxHistorySize: number;
+}
+
+interface OriginalBlockState {
+  id: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+interface ResizeState {
+  id: number | string;
+  handle: ResizeString;
+  startWidth: number;
+  startHeight: number;
+  startX: number;
+  startY: number;
+  originalBlocks?: OriginalBlockState[];
+}
+
+interface DragState {
+  id: number;
+  startX: number;
+  startY: number;
+}
+
+interface SelectionBoxState {
+  startX: number;
+  startY: number;
+  currentX: number;
+  currentY: number;
+}
+
+interface State {
+  pages: Page[];
+  currentPageId: string;
+  mementoManager: MementoManager;
+  isDarkMode: boolean;
+  panelsVisible: boolean;
+  programsPanelWidth: number;
+  clipboard: Block[] | null;
+  programFilter: string;
+  notification: string | null;
+  notificationVisible: boolean;
+  editingPageId: string | null;
+  isShiftPressed: boolean;
+}
+
+type ResizeHandler = (
+  block: Block,
+  e: { percentX: number; percentY: number },
+) => { width: number; height: number; x: number; y: number };
+
+type ResizeString = "nw" | "ne" | "sw" | "se" | "n" | "s" | "e" | "w";
+
+interface AppDispatchEventDetail {
+  state: State;
+  action: import("hyperapp").Action<State>;
+  payload: any;
+  prevState: State;
+}
