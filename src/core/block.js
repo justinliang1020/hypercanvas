1b8cf97405034d93962a53560b2215bdccf3898f
Justin Liang <justinliang1020@gmail.com>
2025-10-19 12:04:36 -0400

    add arrow to link

--- src/core/block.js
+++ src/core/block.js
@@ -221,21 +221,43 @@ export function linkView(link) {
   const distance = Math.sqrt(dx * dx + dy * dy);
   const angle = Math.atan2(dy, dx) * (180 / Math.PI);
 
-  return h("div", {
-    style: {
-      position: "absolute",
-      left: `${parentCenterX}px`,
-      top: `${parentCenterY}px`,
-      width: `${distance}px`,
-      height: "2px",
-      backgroundColor: "#888",
-      transformOrigin: "0 50%",
-      transform: `rotate(${angle}deg)`,
-      pointerEvents: "none",
-      zIndex: "0",
+  const arrowSize = 16;
+  const arrowX = distance / 2 - arrowSize / 2;
+  const arrowY = -(arrowSize / 3) - 2; //TODO: do a better formula
+
+  return h(
+    "div",
+    {
+      style: {
+        position: "absolute",
+        left: `${parentCenterX}px`,
+        top: `${parentCenterY}px`,
+        width: `${distance}px`,
+        height: "2px",
+        backgroundColor: "#888",
+        transformOrigin: "0 50%",
+        transform: `rotate(${angle}deg)`,
+        pointerEvents: "none",
+        zIndex: "0",
+      },
+      key: `${link.parentBlock.id}-${link.childBlock.id}`,
     },
-    key: `${link.parentBlock.id}-${link.childBlock.id}`,
-  });
+    [
+      h("div", {
+        style: {
+          position: "absolute",
+          left: `${arrowX}px`,
+          top: `${arrowY}px`,
+          width: "0",
+          height: "0",
+          borderLeft: `${arrowSize / 2}px solid transparent`,
+          borderRight: `${arrowSize / 2}px solid transparent`,
+          borderBottom: `${arrowSize}px solid black`,
+          transform: "rotate(90deg)",
+        },
+      }),
+    ],
+  );
 }
 
 /**
