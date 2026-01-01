7705aa900a6f36a7deda4f4c59c34744adce8d4c
Justin Liang <justinliang1020@gmail.com>
2026-01-01 12:54:44 -0600

    debugger tpl

---
+++ src/debugger/debuggerScript.tpl.js
@@ -0,0 +1,10 @@
+// template file for debuggerScript.js
+// 1. Run the following command
+// `cp debuggerScript.tpl.js debuggerScript.js`
+// 2. Modify debuggerScript.js to log whatever you want
+/**
+ * @param {State} state
+ */
+export function log(state) {
+  console.table(state);
+}
