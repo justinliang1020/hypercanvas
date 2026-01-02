b2e6eb50ac6a2b3442906c858f6d30d7cc565940
Justin Liang <justinliang1020@gmail.com>
2026-01-02 12:35:35 -0600

    remove dead constants

--- src/core/constants.js
+++ src/core/constants.js
@@ -22,20 +22,6 @@ export const RESIZE_CURSORS = {
   e: "ew-resize",
 };
 
-export const OUTLINE_COLORS = {
-  EDITING: "skyblue",
-  SELECTED: "blue",
-  PREVIEW_SELECTED: "rgba(0, 122, 204, 0.6)", // Semi-transparent blue for preview
-  HOVERING: "blue",
-  INTERACT_MODE: "green",
-};
-
-export const OUTLINE_WIDTHS = {
-  THICK: 4, // For important states
-  MEDIUM: 3, // For secondary states
-  THIN: 2, // For hover states
-};
-
 export const BLOCK_CONTENTS_CLASS_NAME = "block-contents";
 
 const blockContentsWidth = 1200;
@@ -44,10 +30,5 @@ export const BLOCK_PADDING = 70;
 export const DEFAULT_BLOCK_WIDTH = blockContentsWidth + 2 * BLOCK_PADDING;
 export const DEFAULT_BLOCK_HEIGHT = blockContentsHeight + 2 * BLOCK_PADDING;
 export const NEW_CHILD_BLOCK_OFFSET_X = 100;
-export const BLOCK_OPEN_SPACE_PUSH_OFFSET = 10;
-
-export const FONT_SIZES = [
-  8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 48, 64, 72,
-]; // px
 
 export const Z_INDEX_TOP = 100000;
