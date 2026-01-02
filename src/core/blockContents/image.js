786e5a55d5d76db09bebb6b8373a59c89feee0ca
Justin Liang <justinliang1020@gmail.com>
2026-01-01 19:38:37 -0600

    make image blocks draggable

--- src/core/blockContents/image.js
+++ src/core/blockContents/image.js
@@ -2,6 +2,7 @@ import { h, text } from "hyperapp";
 import { getViewportCenterCoordinates } from "../viewport.js";
 import { DEFAULT_BLOCK_WIDTH, DEFAULT_BLOCK_HEIGHT } from "../constants.js";
 import { addImageBlock } from "../block.js";
+import { getCurrentPage } from "../pages.js";
 
 /**
  * @param {State} state
@@ -9,10 +10,14 @@ import { addImageBlock } from "../block.js";
  * @return {import("hyperapp").ElementVNode<State>}
  */
 export function imageContent(state, block) {
+  const currentPage = getCurrentPage(state);
+  const isSelected = currentPage.selectedIds.includes(block.id);
   return h("img", {
     style: {
       width: "100%",
       height: "100%",
+      pointerEvents: "none",
+      boxShadow: isSelected ? "0 0 40px 8px rgba(244, 213, 118, 0.30)" : "",
     },
     src: block.src,
   });
